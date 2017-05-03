f4e59f85a8aba63476819c69122ffdab69a8a8bc
aszlig <aszlig@redmoonstudios.org>
2017-05-03 05:56:48 +0200

    default.nix: Add all gstreamer plugins
    This was the reason audio wasn't working for me so far.
    
    In the long term however I think it would be a good idea to actually
    show an error dialogue with detailed information if that happens.
    
    Signed-off-by: aszlig <aszlig@redmoonstudios.org>

--- default.nix
+++ default.nix
@@ -4,7 +4,16 @@ stdenv.mkDerivation {
   name = "CasterSoundboard";
   src = ./CasterSoundboard;
   nativeBuildInputs = [ qt5.qmakeHook qt5.makeQtWrapper ];
-  buildInputs = [ qt5.qtmultimedia ];
+  buildInputs = [
+    qt5.qtmultimedia
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+  ];
   enableParallelBuilding = true;
-  postInstall = "wrapQtProgram \"$out/bin/$name\"";
+  postInstall = ''
+    wrapQtProgram "$out/bin/$name" \
+      --prefix GST_PLUGIN_SYSTEM_PATH_1_0 : "$GST_PLUGIN_SYSTEM_PATH_1_0"
+  '';
 }
